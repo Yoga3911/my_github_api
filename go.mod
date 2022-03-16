eeae0749d916528862c63a2ae80078b81818210c
Robin Hood <yoga3911@gmail.com>
2022-03-16 13:54:36 +0700

    fasthttp

--- go.mod
+++ go.mod
@@ -1,10 +1,15 @@
 module github_api
 
+// +heroku goVersion go1.18
 go 1.18
 
 require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/gofiber/fiber/v2 v2.29.0
+	github.com/joho/godotenv v1.4.0
 	github.com/klauspost/compress v1.15.1 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.34.0 // indirect
+	github.com/valyala/fasthttp v1.34.0
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/sys v0.0.0-20220315194320-039c03cc5b86 // indirect
 )
--- main.go
+++ main.go
@@ -3,43 +3,66 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
-	"net/http"
+	"os"
 	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+	"github.com/valyala/fasthttp"
 )
 
 type Github struct {
-	SHA string `json:"sha"`
+	SHA    string `json:"sha"`
 	Commit Commit `json:"commit"`
 }
 
 type Commit struct {
-	Committer Committer `json:"committer"` 
-	Message string `json:"message"`
+	Committer Committer `json:"committer"`
+	Message   string    `json:"message"`
 }
 
 type Committer struct {
-	Name string `json:"name"`
-	Date time.Time `json:"date"`
-	NewDate string 
+	Name    string    `json:"name"`
+	Date    time.Time `json:"date"`
 }
 
-func main() {
-	resp, err := http.Get("https://api.github.com/repos/Yoga3911/indogram/commits/main")
-	if err != nil {
-		log.Println(err);
-	}
+type Data struct {
+	SHA       string
+	Message   string
+	Committer string
+	Date      time.Time
+}
 
- 	respData, err := ioutil.ReadAll(resp.Body)
+func getData(c *fiber.Ctx) error {
+	_, resp, err := fasthttp.Get(nil, "https://api.github.com/repos/Yoga3911/indogram/commits/main")
 	if err != nil {
 		log.Println(err)
 	}
 
-	var data Github
-	json.Unmarshal(respData, &data)
+	var github Github
+	json.Unmarshal(resp, &github)
 
-	data.Commit.Committer.NewDate = data.Commit.Committer.Date.Format("02-01-2006")
+	var data Data
+	data.SHA = github.SHA
+	data.Committer = github.Commit.Committer.Name
+	data.Message = github.Commit.Message
+	data.Date = github.Commit.Committer.Date
 
-	fmt.Println(data.Commit.Committer.NewDate)
+	tmp := fmt.Sprintf("%v\n%v\n%v\n%v", data.SHA, data.Committer, data.Message, data.Date)
+
+	return c.Status(fiber.StatusOK).SendString(tmp)
+}
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/api/yoga3911", getData)
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Fatal(app.Listen(":" + os.Getenv("PORT")))
 }
